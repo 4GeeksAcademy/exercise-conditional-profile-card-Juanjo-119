60568785faefabbd491a32f60be4278b7b43ab84
JuanjoVillarejo <jota_villarejo22@hotmail.com>
2025-08-20 14:36:38 +0000

    ternarios correctos mas socialPosition

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,16 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city} - ${variables.country}</h3>
-          <ul class="position-right">
+          <h1>${variables.name == null ? "Lucy" : variables.name} 
+          ${variables.lastName == null ? "Boilett" : variables.lastName}</h1>
+          <h2>${variables.role == null ? "Web Developer" : variables.role}</h2>
+          <h3>${variables.city == null ? "Miami" : variables.city} - 
+          ${variables.country == null ? "USA" : variables.country}</h3>
+          <ul class="${
+            variables.socialMediaPosition == "position-left"
+              ? "position-left"
+              : "position-right"
+          }">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -89,3 +95,6 @@ window.onload = function() {
     });
   });
 };
+/*condicion ? si se cumple : si no se cumple
+${variables.name == null ? "Lucy" : variables.name}
+${variables.name !== null ? variables.name : "Lucy"}*/
